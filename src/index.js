381dddacc2212fef7202d74d1ed1a9f5291c782c
skibyks <skiby_1@mail.ru>
2021-03-14 21:12:51 +0300

    init: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
+module.exports =function reverse (n) {
+    let rev = ""
+    n = String(n)
+    n= n.split('').reverse()
+    for (let i = 0; i < n.length; i++){
+        if(n[i] === '-'){
+            n[i]=""
+          } else{
+           rev+=n[i] 
+          }
+    }
+  return Number(rev)
+  }
   
-}
